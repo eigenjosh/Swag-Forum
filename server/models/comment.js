d19c6b38a8bb97dfda64febe63b4593126481a41
jaime ambriz <jaime.ambriz@yahoo.com>
2017-11-17 13:17:59 -0700

    added object id

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,14 @@
 var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var ObjectId = Schema.Types.ObjectId
 
 var schema = new mongoose.Schema({
     votes: { type: Number, default: 0 },
     body: { type: String, required: true },
     mediaUrl: { type: String },
     createDate: {type: Number, required: true, default: Date.now()},
+    userId: { type: ObjectId, required: true, ref: 'User' },
+    postId: { type: ObjectId, required: true, ref: 'Post' },
     
 })
 
--- server/models/post.js
+++ server/models/post.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var ObjectId = Schema.Types.ObjectId
 
 var schema = new mongoose.Schema({
     votes: { type: Number, default: 0 },
     mediaUrl: { type: String, required: true },
     postTitle: {type: String, required: true},
     createDate: {type: Number, required: true, default: Date.now()},
+    userId: { type: ObjectId, required: true, ref: 'User' }
     
 });
 
