84b14c31b8ecbdad84bad33955d46d19296af2e4
Chase Shank <216cds@gmail.com>
2017-11-17 11:56:24 -0700

    schemas changes

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,7 @@ var schema = new mongoose.Schema({
     username: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
-    createDate: {type: Number, required: true},
+    createDate: {type: Number, required: true, default: Date.now()},
     avatar: {type: String, default: '//placehold.it/200x200'}
 });
 
