39e0b9e81aa0534178ea61e4b2a1f59e35d52474
Josh Eigen <josh.eigen@gmail.com>
2017-11-17 13:30:30 -0800

    moved module.exports to user.js:14

--- server/models/user.js
+++ server/models/user.js
@@ -5,43 +5,42 @@ var ObjectId = Schema.Types.ObjectId
 const SALT_FACTOR = 10
 
 var schema = new mongoose.Schema({
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    createDate: {type: Number, required: true, default: Date.now()},
-    avatar: {type: String, default: '//placehold.it/200x200'}
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  createDate: { type: Number, required: true, default: Date.now() },
+  avatar: { type: String, default: '//placehold.it/200x200' }
 });
+module.exports = mongoose.model('User', schema)
 
 schema.pre('save', function (next) {
-    var user = this;
-    if (!user.isModified('password')) { // if the user is not changing their password or being created
-      return next();
-    }
-    bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
-      if (err) {
-        return next(err);
-      } else {
-        bcrypt.hash(user.password, salt, function (err, hash) {
-          user.password = hash;
-          next();
-        });
-      }
-    });
-  });
-  
-  schema.methods.validatePassword = function (password) {
-    return new Promise((resolve, reject) => {
-      bcrypt.compare(password, this.password, function (err, isMatch) {
-        if (err || !isMatch) {
-          return reject(err);
-        }
-        return resolve(isMatch);
+  var user = this;
+  if (!user.isModified('password')) { // if the user is not changing their password or being created
+    return next();
+  }
+  bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+    if (err) {
+      return next(err);
+    } else {
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        user.password = hash;
+        next();
       });
-    })
-  };
+    }
+  });
+});
+
+schema.methods.validatePassword = function (password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+      if (err || !isMatch) {
+        return reject(err);
+      }
+      return resolve(isMatch);
+    });
+  })
+};
 
 
 
 
-
-module.exports = mongoose.model('User', schema)
