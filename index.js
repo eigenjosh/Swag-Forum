49e1909610ca14d71d52e01d13c661cd487656e4
jaime ambriz <jaime.ambriz@yahoo.com>
2017-11-17 12:32:32 -0700

    auth again

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var server = express()
 var bp = require('body-parser')
 var DBConnect = require('./config/mlab.js')
+var sessions = require('./auth/sessions')
 var port = 3000
 
 //route variables
@@ -11,11 +12,15 @@ var userRoutes = require('./server/routes/user-routes')
 //register Middleware
 server.use(bp.json())
 server.use(bp.urlencoded({extended: true}))
-
+server.use(sessions)
 ///register routes
+server.use(getViewRoutes)
+
+server.use(Authenticate)
+
+server.use(commentRoutes)
 server.use(postRoutes)
 server.use(userRoutes)
-server.use(commentRoutes)
 
 server.listen(port, function(){
     console.log('Server running on port: ', port)
