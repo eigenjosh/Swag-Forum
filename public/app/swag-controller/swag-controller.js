574cbcecac2a55a381070d2240240266ee693b65
Josh Eigen <josh.eigen@gmail.com>
2017-11-17 16:57:40 -0800

    draw function working. posts being dynamically rendered to page (from dummy data)

---
+++ public/app/swag-controller/swag-controller.js
@@ -0,0 +1,43 @@
+function SwagController(){
+    var swagService = new SwagService()
+    var posts = [{
+        votes: 0,
+        mediaUrl: "//placehold.it/200x200",
+        postTitle: "{type: String, required: true}",
+        createDate: Date.now()
+        }]
+    
+    var postFeedElem = document.getElementById('post-feed')
+
+    function getPosts(){
+        swagService.getPosts(drawPosts)
+    }
+
+    function drawPosts(posts){
+        var template = ''
+        for (var i = 0; i<posts.length;i++){
+            var post = posts[i];
+            template += `
+            <div class="col-sm-10 blog-main">
+                <img src="${post.mediaUrl}" alt="" class="col-sm-2">
+                <div class="blog-post">
+                    <h2 class="blog-post-title">${post.postTitle}</h2>
+                    <p class="blog-post-meta">${post.createDate}
+                        <a href="#">${post.user}</a>
+                    </p>
+                </div>
+                <div class="votes">
+                    <span class = "glyphicon glyphicon-arrow-up"></span>
+                    <span class = "vote-count">${post.votes}</span>
+                    <span class = "glyphicon glyphicon-arrow-down"></span>
+                </div>
+            </div>
+            `
+        }
+        postFeedElem.innerHTML = template
+
+
+
+    }
+    drawPosts(posts)
+}
