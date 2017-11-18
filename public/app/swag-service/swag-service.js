99e72a1cc837407cb3a1d595541f833bcc6931da
Josh Eigen <josh.eigen@gmail.com>
2017-11-17 18:19:57 -0800

    modified service

--- public/app/swag-service/swag-service.js
+++ public/app/swag-service/swag-service.js
@@ -1,7 +1,52 @@
 function SwagService(){
-    var baseUrl = ''
+    var baseUrl = 'http://localhost:3000'
     var comments = []
     var posts = []
     var sessionUID = []
+
+    function logError(err){
+        console.error(err)
+    }
+
+    function Post(config){
+        this.postTitle = config.postTitle.value
+        this.mediaUrl= config.mediaUrl.value
+    }
+
+    function Comment(config){
+
+    }
+
+    function getPosts(){
+       debugger 
+        $.get(baseUrl, + '/forum/view/posts')
+        .then(res => {
+            posts = res
+            console.log(posts)
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ getPosts()
     
 }
