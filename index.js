421e0b552d93aa49403c206e89233d1c89ed1bcb
jaime ambriz <jaime.ambriz@yahoo.com>
2017-11-17 17:53:03 -0700

    __dir name

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var postRoutes = require('./server/routes/post-routes')
 var viewRoutes = require('./server/routes/view-routes')
 
 //register Middleware
+server.use(express.static(__dirname + '/public'))
 server.use(bp.json())
 server.use(bp.urlencoded({extended: true}))
 server.use(sessions)
